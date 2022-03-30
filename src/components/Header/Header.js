b3c3c9a4d44cf01860ff502bfe0d893436940d1c
Eng Md Ektiar Hossen <97474187+engektiar@users.noreply.github.com>
2022-03-30 15:01:31 +0600

    amaZoon react bootstrap

---
+++ src/components/Header/Header.js
@@ -0,0 +1,52 @@
+ import React from 'react';
+ import logo from "../../images/logo.png";
+ import {Navbar,Container,NavDropdown,Nav,Form,FormControl} from "react-bootstrap";
+ import "./Header.css";
+import { Button } from 'bootstrap';
+ const Header = () => {
+   return (
+     <div>
+      <Navbar bg="dark" variant={'dark'} expand="lg">
+  <Container fluid>
+    <Navbar.Brand href="#">
+      <img className='logo' src={logo}></img>
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Form className="d-flex header-from">
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+      </Form>
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      > 
+     
+        <Nav.Link  href="#action1">
+        <span className='gust'>Hello Gust</span>
+         <span className='headerLink'>Sing In</span> 
+        </Nav.Link>
+        <Nav.Link href="#action1">
+        <span className='gust'>Return</span>
+         <span className='headerLink'>& orders</span>
+        </Nav.Link>
+        <Nav.Link href="#action1">
+        <span className='gust'>your</span>
+         <span className='headerLink'>prime</span>
+        </Nav.Link>
+          
+      </Nav>
+      
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+     </div>
+   );
+ };
+ 
+ export default Header;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +15,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
